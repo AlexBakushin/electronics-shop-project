535d0842ec9298e7ba45d9949bac2806b20d6175
AlexBakushin <bakushin35@gmail.com>
2023-08-17 15:39:26 +0500

    decision homework 1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item1 = Item("test", 10000, -1)
+    assert item1.calculate_total_price() == -10000
+
+
+def test_apply_discount():
+    item1 = Item('test', 100, 10)
+    Item.pay_rate = 0.1
+    assert item1.apply_discount() == 10
